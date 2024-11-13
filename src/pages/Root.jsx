c541e4a4b2ac30f9fe437b11c2580ad198e98def
Siddiq Sazzad <siddiqsazzad001@gmail.com>
2024-11-13 23:11:15 +0600

    Navbar and Footer Done

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,9 +1,17 @@
+import { Outlet } from "react-router-dom";
+import NavBar from "./navbar/NavBar";
+import Footer from "./footer/Footer";
 
 
 const Root = () => {
      return (
           <div>
-               Hello
+               <NavBar></NavBar>
+               <div className=" min-h-screen">
+                    <Outlet></Outlet>
+               </div>
+
+               <Footer></Footer>
           </div>
      );
 };
